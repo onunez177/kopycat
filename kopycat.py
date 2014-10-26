b5cf902488f26bd5fa75307b723231058ac87469
Max Bond <maxbondconsulting@gmail.com>
2014-10-26 17:04:11 -0600

    Fixed bugs in CLI, wrote more informative help text. Will be ready for 1.0 when I write README.

--- kopycat.py
+++ kopycat.py
@@ -10,16 +10,42 @@ class CLI(Kopy):
     urlFormat = "https://kopy.io/{documentId}#{passphrase}"
     # Should urlFormat support http:// too?
 
-    description = """ Upload and read encrypted or plaintext files from kopy.io. """
+    description = \
+"""\
+Upload and read encrypted or plaintext files from kopy.io.
+
+kopy.io is a pastebin with client-side encryption features. This allows you to
+share sensitive information without fear of it being snooped on or spidered.
+The cipher employed is AES-256-CBC.
+
+kopycat and kopy.io are not suitable for secret information; proprietary
+code snippets, yes. Nuclear launch codes, not so much. gpg is a safer bet
+against determined adversaries.
+"""
 
     examples = \
 """
 Examples:
 
-kopycat "http://kopy.io/75fEl#JpcOUW"
-# Warning: this specifies your passphrase on the command line
+View a link from kopy.io:
 
-kopycat /path/to/file.txt
+    Warning: This exposes your AES passphrase to the anyone connected to the
+    same system.
+
+    kopycat http://kopy.io/75fEl#JpcOUW
+
+View a link securely:
+
+    kopycat http://kopy.io/75fEl -e # Prompts you for the passphrase
+    kopycat http://kopy.io/75fEl -f /path/to/passphrase.txt
+
+Upload a file to kopy.io:
+
+    kopycat /path/to/file.txt
+    kopycat /path/to/sensitive/file.txt -g 
+
+Bugs? Feature requests? Contributions?
+https://www.github.com/xmnr/kopycat
 """
 
     def _chopProtocol(self, url):
@@ -97,8 +123,9 @@ kopycat /path/to/file.txt
     def arguments(self):
 
         parser = argparse.ArgumentParser(description=self.description,
-                                        epilog=self.examples)
-        parser.add_argument("target", default=False,
+                                        epilog=self.examples,
+                                        formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument("target", default=False, nargs="?",
                             help="File to upload or kopy.io URL to download."+ \
                             " If URL contains an anchor, it is taken to be the" + \
                             " passphrase; however, this method is insecure.")
@@ -108,12 +135,16 @@ kopycat /path/to/file.txt
         parser.add_argument("-e", "--encryption", default=False, action="store_true",
                             help="Encrypt a document before uploading, or decrypt" + \
                             " a document after downloading.")
-        parser.add_argument("-f", "--password-file",
+        parser.add_argument("-f", "--passphrase-file",
                             help="Read passphrase from a file.")
         parser.add_argument("-S", "--strip", default=False, action="store_true",
                             help="Strip whitespace from the file.")
         parser.add_argument("-s", "--stdin", default=False, action="store_true",
                             help="Read passphrase from stdin.")
+        parser.add_argument("-g", "--generate-passphrase", default=False,
+                            action="store_true", help="Generate a random " + \
+                            "passphrase from the system's random device. " + \
+                            "(Implies -u.)")
         parser.add_argument("-k", "--keep", type=int, default=600,
                             help="Number of seconds to store the document. " + \
                             "(Default is 600, or 10 minites.)") 
@@ -134,8 +165,11 @@ kopycat /path/to/file.txt
 
         if arguments.stdin:
             passphrase = stdin.read()
-        elif arguments.password_file:
-            passphrase = open(arguments.password_file).read()
+        elif arguments.passphrase_file:
+            passphrase = open(arguments.passphrase_file).read()
+        elif arguments.generate_passphrase:
+            passphrase = self.generatePassword()
+            arguments.sharable = True
         elif arguments.encryption:
             # Requires passphrase but none specified
             passphrase = self.prompt()
@@ -159,8 +193,8 @@ kopycat /path/to/file.txt
         # Nothing to do
         else:
 
-            usage() # For wetware
-            exit(1) # For software
+            usage()
+            exit(1)
 
 if __name__ == "__main__":
     CLI().main()
